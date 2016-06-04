5c2876df34cd1c1ac6447dedeaba0b247b9b0831
unatang <1589410802@qq.com>
2016-06-04 22:12:08 +0800

    绑定事件存在bug

---
+++ 4.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function(){
+    for(var i=1; i<5; i++){    
+    var $p = $("<p><img src=\"images/0" + i + ".jpg\" /></p>");
+    var num = i;
+    $p.mouseenter(function(){      
+        $("#placeholder > img").attr("src", "images/" + num + ".jpg");
+    })
+    $("#content").append($p);
+    }
+   
+    var img = $("<img src=\"images/0.gif\" />");
+    $("#placeholder").append(img);
+   
+   
+   
+})
+     
---
+++ 5.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function(){
+    var $p = $("#content > p");
+    for(var i=0; i<$p.length; i++){
+        $("#content > p").children().eq(i).mouseenter(function(){
+            var source = this.attr("source");
+            var $placeholder = $("#placeholder > img");
+            $placeholder.attr("src", source);
+        });
+    }
+});
