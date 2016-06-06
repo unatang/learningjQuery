67e8e3b5d3a26175f23a24cac78e0bf34d35916a
unatang <1589410802@qq.com>
2016-06-06 16:15:58 +0800

    加深印象练习

---
+++ 6.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function(){
+    $("#content > p").each(function(){
+        $(this).mouseover(function(){
+            $("#placeholder > p > img").attr("src", $(this).attr("source"));
+        });
+    });
+});
