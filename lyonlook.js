bad3a3137530ff77e654ef0f65b80ee4e21a9589
unatang <1589410802@qq.com>
2016-06-21 23:32:04 +0800

    动画练习

---
+++ lyonlook.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function(){
+    $("#lyonlook").css("opacity", "0.5");
+    $("#lyonlook").click(function(){
+        $(this).animate( {left: "300px"}, 2000)
+               .animate( {top: "300px"}, 2000)
+               .slideUp("slow");
+    })
+});
